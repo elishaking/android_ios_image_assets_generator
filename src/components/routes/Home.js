c0410c23db4b62fc91a7477c3bc93b4be4253ca0
King Elisha <ek.chibueze@gmail.com>
2019-10-23 17:22:54 +0100

    dropzone

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
 import './Home.scss';
 
 import NavBar from '../NavBar';
@@ -6,8 +7,25 @@ import NavBar from '../NavBar';
 export default class Home extends Component {
   render() {
     return (
-      <div>
+      <div id="home">
         <NavBar />
+
+        <div className="main">
+          <div className="drop-container">
+            <Dropzone>
+              {
+                ({ getRootProps, getInputProps }) => (
+                  <div {...getRootProps()} className="select">
+                    <input {...getInputProps()} />
+
+                    <p>Select Image</p>
+                    <small>1000x1000 recommended</small>
+                  </div>
+                )
+              }
+            </Dropzone>
+          </div>
+        </div>
       </div>
     )
   }
