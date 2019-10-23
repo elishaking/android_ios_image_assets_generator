486350fd08e1a7f0bfff6b76b1b0030956ea87cd
King Elisha <ek.chibueze@gmail.com>
2019-10-23 18:27:06 +0100

    display images

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -7,7 +7,8 @@ import NavBar from '../NavBar';
 
 export default class Home extends Component {
   state = {
-    dropText: "Select/Drop Image"
+    dropText: "Select/Drop Image(s)",
+    files: []
   };
 
   /**
@@ -15,7 +16,18 @@ export default class Home extends Component {
    * @param {import('react-dropzone').DropEvent} event
    */
   dropped = (files, event) => {
-    console.log(files);
+    files.forEach((file) => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const newFile = {
+          file: e.target.result,
+          name: file.name
+        }
+        this.setState({ files: [newFile, ...this.state.files] });
+      };
+      reader.readAsDataURL(file);
+    });
+
   };
 
   dragEnter = () => {
@@ -23,10 +35,11 @@ export default class Home extends Component {
   };
 
   dragLeave = () => {
-    this.setState({ dropText: "Select/Drop Image" });
+    this.setState({ dropText: "Select/Drop Image(s)" });
   };
 
   render() {
+    const { files } = this.state;
     return (
       <div id="home">
         <NavBar />
@@ -36,18 +49,34 @@ export default class Home extends Component {
             <Dropzone
               onDropAccepted={this.dropped}
               onDragEnter={this.dragEnter}
-              onDragLeave={this.dragLeave} >
+              onDragLeave={this.dragLeave}
+              accept={['image/png', 'image/jpeg']}>
               {
-                ({ getRootProps, getInputProps }) => (
+                ({ getRootProps, getInputProps, isDragReject }) => (
                   <div {...getRootProps()} className="select">
                     <input {...getInputProps()} />
 
                     <p>{this.state.dropText}</p>
                     <small>1000x1000 recommended</small>
+                    {isDragReject && (<small className="error">File type not accepted</small>)}
                   </div>
                 )
               }
             </Dropzone>
+            {
+              files.length > 0 && (
+                <div className="img-display">
+                  {
+                    files.map((file) => (
+                      <div>
+                        <img src={file.file} alt="" />
+                        <p>{file.name} </p>
+                      </div>
+                    ))
+                  }
+                </div>
+              )
+            }
           </div>
         </div>
       </div>
