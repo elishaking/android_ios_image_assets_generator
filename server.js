e112a5062724da9d5e0e45dabea17a04872c104a
King Elisha <ek.chibueze@gmail.com>
2019-10-24 10:14:51 +0100

    comment --unused

--- server.js
+++ server.js
@@ -15,36 +15,21 @@ server.use((req, res, next) => {
   next();
 });
 
-server.post("/android", (req, res) => {
+server.post("/android", async (req, res) => {
   const files = req.body;
-  // console.log(req.body);
-  // for (let i = 0; i < files.length; i++) {
-  const parts = files[0].split(';');
-  const mimType = parts[0].split(':')[1];
-  const imageData = parts[1].split(',')[1];
 
-  const img = new Buffer(imageData, 'base64');
-  const image = { name: Date.now().toString() };
-  sharp(img)
-    .resize(64, 64)
-    .toFile("./images/" + image.name + ".png")
-    .then((v) => {
-      // res.setHeader("Content-Type", "image/png");
-      // res.setHeader("Content-Disposition", `attachment; filename="${image.name}.png"`)
+  for (let i = 0; i < files.length; i++) {
+    const parts = files[0].split(';');
+    const mimType = parts[0].split(':')[1];
+    const imageData = parts[1].split(',')[1];
 
+    const img = new Buffer(imageData, 'base64');
+    const image = { name: Date.now().toString() };
+    await sharp(img)
+      .resize(64, 64)
+      .toFile("./images/" + image.name + ".png");
+  }
 
-    });
-  // .toBuffer()
-  // .then((resizedImageBuffer) => {
-  //   const resizedImageData = resizedImageBuffer.toString('base64');
-  //   const resizedBase64 = `data:${mimType};base64,${resizedImageData}`;
-  //   res.send(resizedBase64);
-  // })
-  // .catch((error) => {
-  //   // error handeling
-  //   res.send(error);
-  // });
-  // }
   const zipOutput = fs.createWriteStream('./zipped/zipOutput.zip');
   zipOutput.on('close', () => {
     console.log(`${archive.pointer()} total bytes`);
